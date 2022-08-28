6220f12697b154772de093b01da21057a5fd1c7f
Yoshin <taigaby0411@gmail.com>
2022-08-29 01:36:25 +0900

    ログイン機能の実装

---
+++ app/Listeners/RegisteredListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Mail\Mailer;
+use App\Models\User;
+
+class RegisteredListener
+{
+    private $mailer;
+    private $eloquent;
+
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct(Mailer $mailer, User $eloquent)
+    {
+        $this->mailer = $mailer;
+        $this->eloquent = $eloquent;
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  \Illuminate\Auth\Events\Registered  $event
+     * @return void
+     */
+    public function handle(Registered $event)
+    {
+        $user = $this->eloquent->findOrFail($event->user->getAuthIdentifier());
+        $this->mailer->raw('会員登録が完了しました', function ($message) use (
+            $user
+        ) {
+            $message->subject('会員登録メール')->to($user->email);
+        });
+    }
+}
