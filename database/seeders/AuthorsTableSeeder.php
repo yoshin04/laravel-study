b4b8ff36b263b73d050ccd7cdf8b5b3376d92815
Yoshin <taigaby0411@gmail.com>
2022-08-31 01:18:11 +0900

    add seeder & factory

---
+++ database/seeders/AuthorsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Author;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AuthorsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Author::factory()
+            ->count(10)
+            ->create();
+    }
+}
